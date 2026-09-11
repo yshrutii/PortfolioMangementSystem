306c2ce58985b3c41f940a9b3c3a8e806a8aa75e
shruti <11yshruti11@gmail.com>
2026-09-11 23:40:06 +0530

    added constructor demo

--- src/main/java/com/portfolio/app/model/Methoddemmo.java
+++ src/main/java/com/portfolio/app/model/Methoddemmo.java
@@ -1,26 +1,18 @@
+ package com.portfolio.app.model;
 
-package com.portfolio.app.model;
+class Student1 {
+    public Student1() {
+        System.out.println("Hi welcome to my class");
+    }
 
-class Student1
-{
-	public Student1() //default constructor
-	{
-		System.out.println("Hi welcome to my class");
-	}
-	
-	public Student1(int x) //para cont
-	{
-		System.out.println(x);
-	}
+    public Student1(int x) {
+        System.out.println(x);
+    }
 }
 
-public class demomethod 
-{
-	public static void main(String[] args)
-	{
-		Student1 s= new Student1();
-		Student1 s1= new Student1(22);
-	}
-
-	}
-
+public class Methoddemmo {
+    public static void main(String[] args) {
+        Student1 s = new Student1();
+        Student1 s1 = new Student1(10);
+    }
+}
